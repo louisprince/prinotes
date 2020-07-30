6f96f737dd5dfa3edfb67409c86f7534ad60d208
Louis Prince <louis@louisprince.co.uk>
2020-07-30 17:39:46 +0100

    More planning

---
+++ sqlmodel.py
@@ -0,0 +1,22 @@
+# Check for exisiting database. If not create tables.
+
+
+# Add user
+
+
+# Check user credentials
+
+
+# Change user password
+
+
+# Add note
+
+
+# Edit note
+
+
+# Delete note
+
+
+# Get user notes
